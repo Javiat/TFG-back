1346c58394d5b4043eaaf3a997ebcc5e76c7fafa
DESKTOP-HPPPRHE\Javi <javimartingar11@gmail.com>
2018-03-07 16:37:56 +0100

    generacion de tokens para un usuario como middleware

--- app/api/users/users.controller.js
+++ app/api/users/users.controller.js
@@ -1,13 +1,13 @@
 'use strict'
 var User=require('./users.model');
 var bcrypt=require('bcrypt-nodejs');
-
+var jwt=require('./jwt');
 
 function saveUser(req,res){
     var user=new User();
     var params=req.body;
     user.name=params.name;
-    user.surname=params.name;
+    user.surname=params.surname;
     user.email=params.email;
     user.password=params.password;
     user.image=params.image;
@@ -48,10 +48,52 @@ function saveUser(req,res){
         res.status(403).send('Los campos email y contraseña son obligatorios');
     }
 }
+function loginUser(req,res){   
+    var params=req.body;
+    var email=params.email;
+    var password=params.password;
+    User.findOne({email:email.toLowerCase()},(err,user)=>{
+       if(err){
+        res.status(404).send({messgae:'Error al hacer el login'});
+       }else{
+           if(!user){
+                res.status(500).send({message:'No existe el usuario'});
+           }else{
+                bcrypt.compare(password,user.password,function(err,check){
+                    if(check){
+                        if(params.gethash){
+                            //devolver un token con jwt
+                            res.status(200).send({
+                                token:jwt.createToken(user)
+                            });
+                        }else{
+                            res.status(404).send({message:'No existe el usuario'});
+                        }
+                    }else{
+                        res.status(404).send({mesage:'No se ha podido identificar'});
+                    }
+                });
+           }
+       }
+    });
 
-function login(req,res){
-    
+}
+function deleteUser(req,res){
+    var user_id=req.params.id;
+    User.findByIdAndRemove(user_id,function(err,User){
+        if(err){
+            res.status(404).send({messgae:'Error'});
+        }else{
+            if(User){
+                res.status(200).send({User:User});
+            }else{
+                res.status(404).send({messgae:'No existe el usuario'});
+            }
+        }
+    });
 }
 module.exports={
-    saveUser
+    saveUser,
+    deleteUser,
+    loginUser
 };
--- app/api/users/users.routes.js
+++ app/api/users/users.routes.js
@@ -2,10 +2,11 @@
 
 var express=require('express');
 var UserController=require('./users.controller');
-
+var md_auth=require('./authenticated');
 var api=express.Router();
 
 
 api.post('/save-user',UserController.saveUser);
-
+api.delete('/delete-user/:id',UserController.deleteUser);
+api.post('/login-user',UserController.loginUser);
 module.exports=api;
